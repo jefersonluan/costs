a4f11ddafd30a1e8e304d7ffb6625941a16dbfc0
Jeferson Luan <jefersonluan05@gmail.com>
2024-01-11 09:46:09 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
+import Projects from "./components/pages/Project";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Container customClass="min_height"><Home /></Container>} />
+        <Route path="/projects" element={<Container customClass="min_height"><Projects /></Container>} />
+        <Route path="/company" element={<Container customClass="min_height"><Company /></Container>} />
+        <Route path="/contact" element={<Container customClass="min_height"><Contact /></Container>} />
+        <Route path="/newproject" element={<Container customClass="min_height"><NewProject /></Container>} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
